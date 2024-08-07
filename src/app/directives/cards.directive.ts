fa739a01025b0d2c369bfb50ad6c3d4db4b2079a
marcosjrs <marcosjrs80@gmail.com>
2024-08-07 17:04:10 +0200

    Probando con directivas de estructura

--- src/app/directives/cards.directive.ts
+++ src/app/directives/cards.directive.ts
@@ -16,12 +16,15 @@ export class CardsDirective {
   cards = input<Card[]>([]);
   templateRef = inject(TemplateRef<any>);
   container = inject(ViewContainerRef);
-  listener = effect(() => {
-    this.container.clear();
-    for (let i = 0; i < this.cards().length; i++) {
-      const context = { $card: this.cards()[i] };
-      this.container.createEmbeddedView(this.templateRef, context);
-    }
-  });
 
+  constructor(){
+    effect(() => {
+      this.container.clear();
+      for (let i = 0; i < this.cards().length; i++) {
+        const context = { $card: this.cards()[i] };
+        this.container.createEmbeddedView(this.templateRef, context);
+      }
+    });
+  }
+  
 }
